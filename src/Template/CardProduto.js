58e93fc94c8eb7b826983d3260cc0eb3963f8cf2
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2022-07-27 15:44:14 -0300

    style: image  Lorem Picsum

--- src/Template/CardProduto.js
+++ src/Template/CardProduto.js
@@ -1,54 +1,48 @@
-import { produtos }  from '../produtos.js'
+import { produtos } from "../produtos.js";
 
-const cardProdutoView = document.getElementById('card-produto') 
+const cardProdutoView = document.getElementById("card-produto");
 
-export default function cardProduto (){
-    
-    produtos.map(produto => {
-        
-        const div = document.createElement('div')
-        div.classList = 'w-40 p-2 text-sm flex flex-col justify-between'
-        const img = document.createElement('img')
-        img.classList = 'rounded'
-        img.setAttribute('src', `${produto.imagem}`)
-        const pNome = document.createElement('p')
-        pNome.classList = 'uppercase  mt-3'
-        pNome.textContent = `${produto.nome}`
-        const pDescricao = document.createElement('p')
-        pDescricao.classList = 'text-gray-400 font-extralight'
-        pDescricao.textContent = `${produto.descricao}`
-        const pTempo = document.createElement('p')
-        pTempo.classList = 'text-gray-400 font-extralight mt-2'
-        pTempo.textContent = `${produto.tempo} min`
-        const iconTempo = document.createElement('i')
-        iconTempo.classList = 'far fa-clock ml-2'
-        const pPreco = document.createElement('p')
-        pPreco.classList = 'mt-2'
-        pPreco.textContent = `R$ ${produto.preco.toFixed(2)}`
-        const iconPreco = document.createElement('i')
-        iconPreco.classList = 'far fa-money-bill-alt ml-2'
-        const btnAdd =  document.createElement('button')
-        btnAdd.classList = 'btn-add bg-yellow-400 w-full uppercase pt-2 pb-1 text-xs text-white rounded-sm outline-none'
-        btnAdd.setAttribute('data-id', `${produto.id}`)
-        btnAdd.textContent = 'ADICIONAR'
-      
+export default function cardProduto() {
+  produtos.map((produto) => {
+    const div = document.createElement("div");
+    div.classList = "w-40 p-2 text-sm flex flex-col justify-between";
+    const img = document.createElement("img");
+    img.classList = "rounded";
+    img.setAttribute("src", `${produto.imagem}`);
+    const pNome = document.createElement("p");
+    pNome.classList = "uppercase  mt-3";
+    pNome.textContent = `${produto.nome}`;
+    const pDescricao = document.createElement("p");
+    pDescricao.classList = "text-gray-500 font-extralight";
+    pDescricao.textContent = `${produto.descricao}`;
+    const pTempo = document.createElement("p");
+    pTempo.classList = "text-gray-500 font-extralight mt-2";
+    pTempo.textContent = `${produto.tempo} min`;
+    const iconTempo = document.createElement("i");
+    iconTempo.classList = "far fa-clock ml-2";
+    const pPreco = document.createElement("p");
+    pPreco.classList = "mt-2";
+    pPreco.textContent = `R$ ${produto.preco.toFixed(2)}`;
+    const iconPreco = document.createElement("i");
+    iconPreco.classList = "far fa-money-bill-alt ml-2";
+    const btnAdd = document.createElement("button");
+    btnAdd.classList =
+      "btn-add bg-yellow-400 w-full uppercase pt-2 pb-1 text-xs text-white rounded-sm outline-none";
+    btnAdd.setAttribute("data-id", `${produto.id}`);
+    btnAdd.textContent = "ADICIONAR";
 
+    pTempo.appendChild(iconTempo);
+    pPreco.appendChild(iconPreco);
 
-        pTempo.appendChild(iconTempo)
-        pPreco.appendChild(iconPreco)
-
-        div.appendChild(img)
-        div.appendChild(pNome)
-        div.appendChild(pDescricao)
-        div.appendChild(pTempo)
-        div.appendChild(pPreco)
-        div.appendChild(btnAdd)
-        cardProdutoView.appendChild(div)
-
-    })
+    div.appendChild(img);
+    div.appendChild(pNome);
+    div.appendChild(pDescricao);
+    div.appendChild(pTempo);
+    div.appendChild(pPreco);
+    div.appendChild(btnAdd);
+    cardProdutoView.appendChild(div);
+  });
 }
-
-
 
 /* <section class='produtos'>
     <div id='card-produto' class='grid grid-cols-2 gap-4 sm:flex sm:flex-wrap'>
